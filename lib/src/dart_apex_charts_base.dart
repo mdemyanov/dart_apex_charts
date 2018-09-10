cc7579fcbff231277acdbab98636580a6cae5a02
mdemyanov <Se1M3Dead>
2018-09-10 17:32:43 +0300

    Check errors, add some examples

--- lib/src/dart_apex_charts_base.dart
+++ lib/src/dart_apex_charts_base.dart
@@ -13,21 +13,21 @@ class ApexCharts {
         <dynamic>[querySelector(selector), _jsOptions]);
   }
 
-  JsObject _callMethod(String methodName,
+  dynamic _callMethod(String methodName,
           [List<dynamic> options = const <dynamic>[]]) =>
-      JsObject(_chart.callMethod(methodName, options) as JsFunction);
+      _chart.callMethod(methodName, options);
 
   /// The render() method is responsible for drawing the chart on the page.
   ///
   /// It is the primary method which has to be called after configuring the
   /// options.
-  Future<JsObject> render() async => _callMethod('render');
+  Future<dynamic> render() async => _callMethod('render');
 
   ///Allows you to update the series array overriding the existing one.
   ///
   ///If you want to append dataset to existing series, use the appendData()
   ///method
-  JsObject updateSeries(
+  dynamic updateSeries(
           [List<Map> newSeries = const [], bool animate = true]) =>
       _callMethod(
           'updateSeries', <dynamic>[JsObject.jsify(newSeries), animate]);
@@ -36,7 +36,7 @@ class ApexCharts {
   ///
   ///If you have existing multiple series, provide the new array in the same
   ///indexed manner.
-  JsObject appendData(List<Map> newSeries) =>
+  dynamic appendData(List<Map> newSeries) =>
       _callMethod('appendData', <dynamic>[JsObject.jsify(newSeries)]);
 
   ///Allows you to update the configuration object by passing the options as
@@ -44,7 +44,7 @@ class ApexCharts {
   ///
   ///The new config object is merged with the existing config object preserving
   ///the existing configuration.
-  JsObject updateOptions(Map newOptions, [bool animate = true]) => _callMethod(
+  dynamic updateOptions(Map newOptions, [bool animate = true]) => _callMethod(
       'updateOptions', <dynamic>[JsObject.jsify(newOptions), animate]);
 
   ///The addText() method can be used to draw text after chart is rendered.
@@ -52,18 +52,18 @@ class ApexCharts {
   ///The position of the text is controlled by x and y parameters. You can also
   ///provide the styling of the text as well change background color of the rect
   /// around text.
-  JsObject addText(Map options) =>
+  dynamic addText(Map options) =>
       _callMethod('addText', <dynamic>[JsObject.jsify(options)]);
 
   ///The addXaxisAnnotation() method can be used to draw annotations after chart is rendered.
-  JsObject addXaxisAnnotation(Map options) =>
+  dynamic addXaxisAnnotation(Map options) =>
       _callMethod('addXaxisAnnotation', <dynamic>[JsObject.jsify(options)]);
 
   ///The addYaxisAnnotation() method can be used to draw annotations after chart is rendered.
-  JsObject addYaxisAnnotation(Map options) =>
+  dynamic addYaxisAnnotation(Map options) =>
       _callMethod('addYaxisAnnotation', <dynamic>[JsObject.jsify(options)]);
 
   ///The addPointAnnotation() method can be used to draw annotations after chart is rendered.
-  JsObject addPointAnnotation(Map options) =>
+  dynamic addPointAnnotation(Map options) =>
       _callMethod('addPointAnnotation', <dynamic>[JsObject.jsify(options)]);
 }
